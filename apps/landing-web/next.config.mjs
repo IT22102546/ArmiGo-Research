a4980f42a90194c10fedffcf5fd81b2020453b90
Sanjana Nimesh <sanjana.nim2001@gmail.com>
2026-03-10 14:45:52 +0530

    Conifguration fixed

--- apps/landing-web/next.config.mjs
+++ apps/landing-web/next.config.mjs
@@ -1,10 +1,16 @@
-// apps/landing-web/next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Disable SWC if issues persist
-  swcMinify: false,
-  
-  // Your existing rewrites
+  output: 'standalone',
+  poweredByHeader: false,
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'armigorehab.com',
+      },
+    ],
+  },
   async rewrites() {
     const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8002";
     return {
